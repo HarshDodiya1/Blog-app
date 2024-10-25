a0ea378adc57113fe72a0995ea3628eed83891da
HarshDodiya1 <dodiyaharsh999@gmail.com>
2024-10-25 09:07:02 +0530

    added comments logic to the comments section

--- client/src/Components/Comment.jsx
+++ client/src/Components/Comment.jsx
@@ -9,19 +9,16 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
   const [user, setUser] = useState({});
   const [isEditing, setIsEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(comment.content);
-  console.log("what is in the comment? after submitting", comment)
   const { currentUser } = useSelector((state) => state.user);
   useEffect(() => {
     const getUser = async () => {
       try {
         const res = await fetch(`/api/user/${comment.userId}`);
-        console.log("again same issue with this: ", res)
         const data = await res.json();
         if (res.ok) {
           setUser(data);
         }
       } catch (error) {
-        console.log("error.message");
       }
     };
     getUser();
@@ -48,7 +45,6 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
         onEdit(comment, editedContent);
       }
     } catch (error) {
-      console.log(error.message);
     }
   };
   return (
