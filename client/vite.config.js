03790dd78703adb4b34d657b0aa5579f946b0b1a
HarshDodiya1 <dodiyaharsh999@gmail.com>
2024-12-11 19:04:04 +0530

    Updated Vite configuration for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,17 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    build: {
+      sourcemap: process.env.NODE_ENV !== "production",
+      outDir: "dist",
+      chunkSizeWarningLimit: 500,
+    },
     proxy: {
       "/api": {
         target:
           process.env.NODE_ENV === "production"
-            ? "https://quillier-backend.vercel.app/"
-            : "http://localhost:3000",
+            ? process.env.VITE_API_BASE_PROD
+            : process.env.VITE_API_BASE_DEV,
         secure: false,
       },
     },
